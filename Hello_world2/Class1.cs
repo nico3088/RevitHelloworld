319b735190567702e311efc4c66abeee0216ca86
Nicolas Garnero <nicogarnero1@outlook.com>
2023-03-13 10:02:28 -0300

    Add project files.

---
+++ Hello_world2/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+
+namespace Hello_world2
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "BIEN HECHAAAAAAAA");
+            return Autodesk.Revit.UI.Result.Succeeded;  
+        }
+    }
+}
